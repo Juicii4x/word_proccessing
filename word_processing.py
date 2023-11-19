72dfb4f69d154d8780e4a8c94586bb752e4347ae
Juicii <87665951+Juicii4x@users.noreply.github.com>
2023-11-19 14:32:31 +0200

    shandis

--- word_processing.py
+++ word_processing.py
@@ -2,5 +2,22 @@
 Beginning of psychological trauma
 """
 
-text = open("words.txt", "r")
-print(text.read())
+
+def get_words(file):
+    """
+    function returns a list of all words from the read txt file
+    args:: file - file directo
+    returns:: a list of words...
+    """
+    f = open("words.txt", "r")
+    words = f.read()
+    f.close()
+    return words
+
+
+def cleaning_words(words):
+    """
+    function lowers all words
+    args:: words list
+    returns:: lowers words ready from processing
+    """
